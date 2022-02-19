602f306ec1d3a2f19e9113ccbb6670f99f49ff78
minho <dnjsalsgh123@gmail.com>
2022-02-19 20:15:24 +0900

    카드 안 content bootstrap 에 custom css 적용

--- src/component/shared/Content.js
+++ src/component/shared/Content.js
@@ -5,16 +5,17 @@ import userdata from "../../data/userdata.json";
 const Content = () => {
   return (
     <div class={styles.background}>
-      {/* <div class="container col-md-6 mt-2"> */}
-      <div className={styles.card_background}>
-        {/* 컨탠츠 3개 들어가는 곳 */}
-        {userdata["data"].map((item) => {
-          return (
-            <div class={styles.flat_card}>
-              <ContentDetail data={item} />
-            </div>
-          );
-        })}
+      <div class="container col-md-6 mt-2">
+        <div className={styles.card_background}>
+          {/* 컨탠츠 3개 들어가는 곳 */}
+          {userdata["data"].map((item) => {
+            return (
+              <div class={styles.flat_card}>
+                <ContentDetail data={item} />
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
--- src/component/shared/ContentDetail.js
+++ src/component/shared/ContentDetail.js
@@ -1,4 +1,5 @@
 import userdata from "../../data/userdata.json";
+import styles from "./ContentDetail.module.css";
 
 const ContentDetail = ({ data }) => {
   return (
@@ -6,21 +7,23 @@ const ContentDetail = ({ data }) => {
       <div class="text-center">
         <img width="200" height="250" alt="contentpull" src={data.imgsrc} />
       </div>
-      <div class="d-flex justify-content-between">
+      <div class="d-flex justify-content-between ">
         <div>남은 시간: 75:00</div>
         <div>조회수 1085</div>
       </div>
-      <div class="d-flex justify-content-between">
-        <div>{data.title}</div>
+      <div class="d-flex justify-content-between pt-2 ">
+        <div className={styles.result_title}>{data.title}</div>
       </div>
       <div class="d-flex justify-content-between">
-        <div>{data.location}</div>
+        <div className={styles.result_location}>{data.location}</div>
       </div>
       <div class="d-flex justify-content-end">
-        <div>현재가격: {data.price}</div>
+        <div className={styles.result_price}>현재가격: {data.price}</div>
       </div>
       <div class="d-flex justify-content-end">
-        <div>즉시구매가격: {data.immediately}</div>
+        <div className={styles.result_price}>
+          즉시구매가격: {data.immediately}
+        </div>
       </div>
     </div>
   );
