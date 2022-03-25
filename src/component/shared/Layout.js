42e178fe7f800d8c052ca106f42cd0411fc8bd5d
kang junho <0320kangk@naver.com>
2022-03-25 20:24:14 +0900

    pullrequest Basket

--- src/component/shared/Layout.js
+++ src/component/shared/Layout.js
@@ -3,12 +3,18 @@ import Header from "./Header";
 import Content from "./Content";
 import SubHeader from "./SubHeader";
 import SideBar from "./SideBar";
+import Basket from "./Basket";
 
 const Layout = () => {
   return (
     <div>
       <Header />
-      <SideBar />
+      <div className="d-flex">
+        <SideBar />
+
+        
+        <Basket />
+      </div>
     </div>
   );
 };
