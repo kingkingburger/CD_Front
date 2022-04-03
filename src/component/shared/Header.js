ef5c0178e4e81bb30eab31f48f61f01638998282
minho <dnjsalsgh123@gmail.com>
2022-04-03 13:33:02 +0900

    로그인 구현하기, 로그인 하면 header에 이름 나오게끔 하기

--- src/component/shared/Header.js
+++ src/component/shared/Header.js
@@ -1,15 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "../css/Header.module.css";
 import font from "../css/Font.module.css";
 
 const Header = () => {
   const [isActive, setIsActive] = useState(false);
+  const [isLogin, setIsLogin] = useState(false); //로그인 관리
   const onClick = () => setIsActive(!isActive);
-  const img_click = () => {
-    alert("이미지 클릭됨");
-  };
+  const img_click = () => {};
 
+  useEffect(() => {
+    if (sessionStorage.getItem("name") === null) {
+      // sessionStorage 에 name 라는 key 값으로 저장된 값이 없다면
+    } else {
+      // sessionStorage 에 name 라는 key 값으로 저장된 값이 있다면
+      // 로그인 상태 변경
+      setIsLogin(true);
+    }
+  });
   return (
     <nav className="navbar-expand-xxl navbar-dark bg-danger float-left">
       <div className="container px-5 ">
@@ -58,9 +66,16 @@ const Header = () => {
               <li></li>
               <div className="d-flex">
                 <li className="nav-item mx-1 d-flex align-items-center">
-                  <Link to={`/Sign`} className="nav-link text-white">
-                    로그인
-                  </Link>
+                  {/* 로그인이 되어있다면 */}
+                  {isLogin ? (
+                    <Link to={`/MyPage`} className="nav-link text-white">
+                      {sessionStorage.getItem("name")}
+                    </Link>
+                  ) : (
+                    <Link to={`/Sign`} className="nav-link text-white">
+                      로그인
+                    </Link>
+                  )}
                 </li>
                 <li className="nav-item mx-1 d-flex align-items-center">
                   <Link to={`/BasketPage`} className="nav-link text-white">
--- src/component/shared/Sign.js
+++ src/component/shared/Sign.js
@@ -1,6 +1,75 @@
 import { Link } from "react-router-dom";
 import font from "../css/Font.module.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 const SignUp = () => {
+  const [text, setText] = useState({ email: "", passwd: "" });
+  const onChange = (e) => {
+    const { value } = e.target;
+    setText({ ...text, [e.target.name]: value });
+  };
+
+  const url = "http://localhost:8080/api/login";
+  // const throw_data = () => {
+  //   axios.post(url, text).then((res) => {
+  //     if(res.data)
+  //   });
+  // };
+  const [inputId, setInputId] = useState("");
+  const [inputPw, setInputPw] = useState("");
+
+  const handleInputId = (e) => {
+    setInputId(e.target.value);
+  };
+
+  const handleInputPw = (e) => {
+    setInputPw(e.target.value);
+  };
+
+  const onClickLogin = () => {
+    console.log("click login");
+    console.log("ID : ", inputId);
+    console.log("PW : ", inputPw);
+    axios
+      .post("http://localhost:8080/api/login", {
+        email: inputId,
+        passwd: inputPw,
+      })
+      .then((res) => {
+        console.log(res);
+        console.log("res.data.userId :: ", res.data.userId);
+        console.log("res.data.msg :: ", res.data.msg);
+        if (res.data.email === undefined) {
+          // id 일치하지 않는 경우 userId = undefined, msg = '입력하신 id 가 일치하지 않습니다.'
+          console.log("======================", res.data.msg);
+          alert("입력하신 id 가 일치하지 않습니다.");
+        } else if (res.data.email === null) {
+          // id는 있지만, pw 는 다른 경우 userId = null , msg = undefined
+          console.log(
+            "======================",
+            "입력하신 비밀번호 가 일치하지 않습니다."
+          );
+          alert("입력하신 비밀번호 가 일치하지 않습니다.");
+        } else if (res.data.email === inputId) {
+          // id, pw 모두 일치 userId = userId1, msg = undefined
+          console.log("======================", "로그인 성공");
+          sessionStorage.setItem("user_id", inputId); // sessionStorage에 id를 user_id라는 key 값으로 저장
+          sessionStorage.setItem("name", res.data.name); // sessionStorage에 id를 user_id라는 key 값으로 저장
+        }
+        // 작업 완료 되면 페이지 이동(새로고침)
+        document.location.href = "/";
+      })
+      .catch();
+  };
+
+  // useEffect(() => {
+  //   axios
+  //     .get("/user_inform/login")
+  //     .then((res) => console.log(res))
+  //     .catch();
+  // }, []);
+
   return (
     <div className="container mt-4 d-flex justify-content-center">
       <div className="d-flex flex-column">
@@ -9,7 +78,7 @@ const SignUp = () => {
         </h1>
         <span className="p">회원정보를 입력해주세요.</span>
 
-        <form action="/action_page.php" style={{ width: "400px" }}>
+        <div style={{ width: "400px" }}>
           <div className="mb-3 mt-3">
             <div className="input-group input-group-lg">
               <div
@@ -27,6 +96,9 @@ const SignUp = () => {
                 type="email"
                 className="form-control"
                 placeholder="Enter email"
+                name="input_id"
+                value={inputId}
+                onChange={handleInputId}
               />
             </div>
           </div>
@@ -47,6 +119,9 @@ const SignUp = () => {
                 type="password"
                 className="form-control"
                 placeholder="Enter password"
+                name="input_pw"
+                value={inputPw}
+                onChange={handleInputPw}
               />
             </div>
           </div>
@@ -55,10 +130,14 @@ const SignUp = () => {
             회원이 아니신가요?
           </Link>
 
-          <button type="submit" className="mt-5 p-3 w-100 btn btn-primary">
+          <button
+            type="button"
+            className="mt-5 p-3 w-100 btn btn-primary"
+            onClick={onClickLogin}
+          >
             확인
           </button>
-        </form>
+        </div>
       </div>
     </div>
   );
